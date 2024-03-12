a90396834760eec780ed7cd5145887d7635df59f
mominurr <mominurr518@gmail.com>
2024-03-13 03:57:30 +0600

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='1.0.0',
     author='Mominur Rahman',
     author_email='mominurr518@email.com',
-    description='A simple tool to verify working proxies',
+    description='A simple package to verify working proxies',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/mominurr/free_proxy_verifyer',
