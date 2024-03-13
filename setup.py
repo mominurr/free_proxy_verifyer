d41b3d8350a3afc365d49d190788077e10e9aaee
mominurr <mominurr518@gmail.com>
2024-03-13 22:20:42 +0600

    update

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='free_proxy_verifyer',
-    version='1.0.1',
+    version='1.1.1',
     author='Mominur Rahman',
     author_email='mominurr518@email.com',
-    description='A simple package to verify working proxies',
+    description='A simple package to verify http and https working proxies.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/mominurr/free_proxy_verifyer',
